31cc29873965da0ca514219217ab6bada9330e04
Leonidas Kollaros <lkollaro@netcompany-intrasoft.com>
2022-10-18 11:11:52 +0300

    a

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -12,8 +12,18 @@ pipeline {
     stage('tester') {
       steps {
         script {
+          properties([
+            parameters([
+              booleanParam(
+                defaultValue: true, 
+                  description: '', 
+                  name: 'doDryRun'
+                )
+
+            ])
+          ])
             echo "try to connect with credentials"
-            if 
+//            if 
             withCredentials([string(credentialsId: "leo.vm_pass", variable: 'SSHPASS')]){
                 sh 'sshpass -e ssh leo@192.168.56.102 -t "export WLST_LOC="/home/leo/Documents/wlcourse/Oracle/Middleware/Oracle_Home/oracle_common/common/bin"; cd /home/leo/Documents/wlcourse/testscripts/testing; ./manage.sh ms1 stop"'
             }
