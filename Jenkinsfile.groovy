26a8d97705b70be84cad7dce5af0fb70d38820b5
Leonidas Kollaros <lkollaro@netcompany-intrasoft.com>
2022-10-18 11:21:17 +0300

    c

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -13,7 +13,7 @@ pipeline {
       steps {
         script {
             echo "try to connect with credentials"
-            echo params.DryRun 
+            echo String.valueof(params.DryRun) 
             withCredentials([string(credentialsId: "leo.vm_pass", variable: 'SSHPASS')]){
                 sh 'sshpass -e ssh leo@192.168.56.102 -t "export WLST_LOC="/home/leo/Documents/wlcourse/Oracle/Middleware/Oracle_Home/oracle_common/common/bin"; cd /home/leo/Documents/wlcourse/testscripts/testing; ./manage.sh ms1 stop"'
             }
