c681cc2508f6558d38659f18adfa5d6970624772
Leonidas Kollaros <lkollaro@netcompany-intrasoft.com>
2022-10-18 15:34:12 +0300

    δ

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -12,15 +12,17 @@ pipeline {
     stage('tester') {
       steps {
         script {
+            def action = params.Action
             echo "try to connect with credentials"
+            echo "${action}"
             if (params.DryRun){
                 withCredentials([string(credentialsId: "leo.vm_pass", variable: 'SSHPASS')]){
-                    sh 'sshpass -e ssh leo@192.168.56.102 -t "export WLST_LOC="/home/leo/Documents/wlcourse/Oracle/Middleware/Oracle_Home/oracle_common/common/bin"; cd /home/leo/Documents/wlcourse/testscripts/testing; ./manage.sh ms1 stop dryRun"'
+                    sh 'sshpass -e ssh leo@192.168.56.102 -t /home/leo/Documents/wlcourse/testscripts/testing; ./manage.sh ms1 '+ action +' dryRun'
                 } 
             }
             else{
                 withCredentials([string(credentialsId: "leo.vm_pass", variable: 'SSHPASS')]){
-                    sh 'sshpass -e ssh leo@192.168.56.102 -t "export WLST_LOC="/home/leo/Documents/wlcourse/Oracle/Middleware/Oracle_Home/oracle_common/common/bin"; cd /home/leo/Documents/wlcourse/testscripts/testing; ./manage.sh ms1 stop"'
+                    sh 'sshpass -e ssh leo@192.168.56.102 -t /home/leo/Documents/wlcourse/testscripts/testing/manage.sh ms1 '+action
                 }
             }
         }
@@ -28,3 +30,35 @@ pipeline {
     }
    }
 }
+
+// pipeline {
+//     agent any
+
+//     options {
+//       disableResume()
+//       buildDiscarder(logRotator(numToKeepStr: '30'))
+//       timestamps()
+//       timeout(time: 2, unit: 'HOURS')
+//     }
+
+//    stages {
+//     stage('tester') {
+//       steps {
+//         script {
+//             echo "try to connect with credentials"
+//             if (params.DryRun){
+//                 withCredentials([string(credentialsId: "leo.vm_pass", variable: 'SSHPASS')]){
+//                     sh 'sshpass -e ssh leo@192.168.56.102 -t "export WLST_LOC="/home/leo/Documents/wlcourse/Oracle/Middleware/Oracle_Home/oracle_common/common/bin"; cd /home/leo/Documents/wlcourse/testscripts/testing; ./manage.sh ms1 stop dryRun"'
+//                 } 
+//             }
+//             else{
+//                 action = "${params.Action}"
+//                 withCredentials([string(credentialsId: "leo.vm_pass", variable: 'SSHPASS')]){
+//                     sh 'sshpass -e ssh leo@192.168.56.102 -t "export WLST_LOC="/home/leo/Documents/wlcourse/Oracle/Middleware/Oracle_Home/oracle_common/common/bin"; cd /home/leo/Documents/wlcourse/testscripts/testing; ./manage.sh ms1 $action"'
+//                 }
+//             }
+//         }
+//       }
+//     }
+//    }
+// }
